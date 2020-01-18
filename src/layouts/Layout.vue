5654e599423c6e7e5437256b42b9ded23d1e06ef
Memo Chou <memochou1993@gmail.com>
2020-01-19 02:45:33 +0800

    Move TheSearchBox component from Index page to Layout

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,13 +1,46 @@
 <template>
   <q-layout>
     <q-page-container>
-      <router-view />
+      <div
+        class="row window-height justify-center items-center"
+      >
+        <div
+          class="col-md-6 col-sm-8 col-xs-10 q-py-xl"
+        >
+          <div
+            class="q-mb-lg"
+            @click="refresh()"
+          >
+            <div
+              class="text-center text-h5 cursor-pointer"
+            >
+              Art & Architecture Thesaurus
+            </div>
+          </div>
+          <TheSearchBox
+            class="q-mb-lg"
+          />
+          <router-view
+            class="q-mb-lg"
+          />
+        </div>
+      </div>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
+import TheSearchBox from '../components/TheSearchBox';
+
 export default {
   name: 'Layout',
+  components: {
+    TheSearchBox,
+  },
+  methods: {
+    refresh() {
+      window.location.href = '/';
+    },
+  },
 };
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,42 +1,16 @@
 <template>
   <div>
-    <div
-      class="row window-height justify-center items-center"
-    >
-      <div
-        class="col-md-6 col-sm-8 col-xs-10 q-py-xl"
-      >
-        <div
-          class="q-mb-lg text-center text-h5 cursor-pointer"
-          @click="refresh()"
-        >
-          Art & Architecture Thesaurus
-        </div>
-        <TheSearchBox
-          class="q-mb-lg"
-        />
-        <SubjectList
-          class="q-mb-lg"
-        />
-      </div>
-    </div>
+    <SubjectList />
   </div>
 </template>
 
 <script>
-import TheSearchBox from '../components/TheSearchBox';
 import SubjectList from '../components/SubjectList';
 
 export default {
   name: 'Index',
   components: {
-    TheSearchBox,
     SubjectList,
-  },
-  methods: {
-    refresh() {
-      window.location.href = '/';
-    },
   },
 };
 </script>
